7f4fb52931f5caf6cf001b50fa257f5ce653175e
tgrlp4a <164776557+tgrlp4a@users.noreply.github.com>
2024-05-27 10:16:25 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Start Nginx
-echo "Starting Nginx..."
-/usr/sbin/nginx -g 'daemon off;'
-
 # Start Wazuh agent
 echo "Starting Wazuh agent..."
 /etc/init.d/wazuh-agent start
@@ -13,10 +9,12 @@ if [ $? -ne 0 ]; then
   echo "Failed to start Wazuh agent"
   echo "Checking Wazuh logs for errors..."
   tail -n 50 /var/ossec/logs/ossec.log
-  echo "Checking Wazuh agent status..."
-  /etc/init.d/wazuh-agent status
   exit 1
 fi
 
 # Tail Wazuh logs in the background
 tail -f /var/ossec/logs/ossec.log &
+
+# Start Nginx
+echo "Starting Nginx..."
+exec "$@"
